1187a7aa4f20a00dc52dc580dcfcd4fefc3bd644
klstein7 <klstein7@gmail.com>
2025-03-17 11:58:47 -0600

    use env

--- src/pusher/client.ts
+++ src/pusher/client.ts
@@ -1,7 +1,9 @@
 import PusherClient from "pusher-js";
 
-const pusherClient = new PusherClient(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
-  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+import { env } from "~/env";
+
+const pusherClient = new PusherClient(env.NEXT_PUBLIC_PUSHER_KEY, {
+  cluster: env.NEXT_PUBLIC_PUSHER_CLUSTER,
   forceTLS: true,
 });
 
